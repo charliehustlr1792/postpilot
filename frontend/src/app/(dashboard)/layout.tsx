79355b0a237f78c40943fc407b39bea1bbc31f77
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2025-09-22 00:32:11 +0530

    feat:hero section(ongoing)

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div className='flex flex-col min-h-screen w-full'>
-      <div className='flex w-full h-[90px] px-[70px] py-[32px] bg-[#F3EFEC] items-center justify-between gap-4 p-4'>
+      <div className='fixed left-0 right-0 top-0 flex w-full h-[90px] px-[70px] py-[32px] bg-[#F3EFEC] items-center justify-between gap-4 p-4'>
         <Logo/>
         <Navbar/>
         <LoginButton/>
